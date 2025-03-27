481fa8cb436fcb69a10975144e11f050fea05d96
Benexl <benextempest@gmail.com>
2025-03-27 12:54:49 +0300

    fix: switch to body parser module for ussd to work

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,15 @@ const cors = require("cors");
 const { initDB } = require("./database");
 const ussdRouter = require("./routes/ussd");
 const apiRouter = require("./routes/api");
+const bodyParser = require("body-parser");
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 app.use("/ussd", ussdRouter);
 app.use("/api", apiRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(5000, () => {
   console.log("[INFO]: Server has started at https://localhost:5000");
