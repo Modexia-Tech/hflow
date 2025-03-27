0bd1d25e1c72de7629ac3c7675c5f6d677e3ffde
Benexl <benextempest@gmail.com>
2025-03-27 11:26:10 +0300

    feat: add user validation schemas for new user registration and transactions

---
+++ backend/schemas/user.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+const newUserSchema = Joi.object({
+  phone: Joi.string()
+    .regex(/^2547\d{8}$/)
+    .required(),
+  fullName: Joi.string().required(),
+  pin: Joi.string().length(4).required(),
+});
+
+const updateUserSchema = Joi.object({
+  phone: Joi.string().regex(/^2547\d{8}$/),
+  fullName: Joi.string(),
+  pin: Joi.string().length(4),
+  failedAttempts: Joi.number(),
+});
+
+const userTransactionSchema = Joi.object({
+  phone: Joi.string()
+    .regex(/^2547\d{8}$/)
+    .required(),
+  pin: Joi.string().length(4).required(),
+});
+module.exports = { newUserSchema, userTransactionSchema };
