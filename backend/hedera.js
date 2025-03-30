3d44cb2adb3886be93144e0c5111a16cfbb19732
Benexl <benextempest@gmail.com>
2025-03-30 10:29:52 +0300

    chore: remove unused files

--- backend/hedera.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require("@hashgraph/sdk");
-
-const hederaClient = Client.forTestnet();
-hederaClient.setOperator(
-  process.env.HEDERA_OPERATOR_ID,
-  process.env.HEDERA_OPERATOR_KEY,
-);
